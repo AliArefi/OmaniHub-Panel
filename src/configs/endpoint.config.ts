b6e478113066337ab09b0b3bfb53cb9e9f8061c7
milad <mihancivil@gmail.com>
2026-03-15 16:05:40 +0330

    change api address

--- src/configs/endpoint.config.ts
+++ src/configs/endpoint.config.ts
@@ -1,5 +1,6 @@
 
-export const apiPrefix = 'https://admin.omanihub.com/api/1'
+// export const apiPrefix = 'https://admin.omanihub.com/api/1'
+export const apiPrefix = 'https://api.iranronagh.ir/api/1'
 
 const endpointConfig = {
     signIn: '/auth/login',
