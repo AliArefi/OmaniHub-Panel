6d396ed53e4b3cc933a812de15185f8a96f4640b
Mohsen Robatjazi <mohroba@gmail.com>
2026-09-18 18:26:10 +0330

    Translate shared theme and access states

--- src/views/others/AccessDenied/AccessDenied.tsx
+++ src/views/others/AccessDenied/AccessDenied.tsx
@@ -1,15 +1,17 @@
 import Container from '@/components/shared/Container'
 import SpaceSignBoard from '@/assets/svg/SpaceSignBoard'
+import useTranslation from '@/utils/hooks/useTranslation'
 
 const AccessDenied = () => {
+    const { t } = useTranslation()
     return (
         <Container className="h-full">
             <div className="h-full flex flex-col items-center justify-center">
                 <SpaceSignBoard height={280} width={280} />
                 <div className="mt-10 text-center">
-                    <h3 className="mb-2">Access Denied!</h3>
+                    <h3 className="mb-2">{t('accessDenied.title')}</h3>
                     <p className="text-base">
-                        You have no permission to visit this page
+                        {t('accessDenied.message')}
                     </p>
                 </div>
             </div>
